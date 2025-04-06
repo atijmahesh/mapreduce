b8b6410b2c8427a6e8a8d6560fe1f81beed4a98b
Atij Mahesh <atijmahesh914@gmail.com>
2025-04-05 19:21:06 -0700

    Update master.go

--- mapreduce/master.go
+++ mapreduce/master.go
@@ -1,14 +1,15 @@
 package mapreduce
 
-import "container/list"
-import "fmt"
-
+import (
+	"container/list"
+	"fmt"
+	"sync"
+)
 
 type WorkerInfo struct {
 	address string
 	// You can add definitions here.
 }
-
 
 // Clean up all workers by sending a Shutdown RPC to each one of them Collect
 // the number of jobs each work has performed.
@@ -29,6 +30,56 @@ func (mr *MapReduce) KillWorkers() *list.List {
 }
 
 func (mr *MapReduce) RunMaster() *list.List {
-	// Your code here
+	var wg sync.WaitGroup
+	workerChannel := make(chan string, 10)
+
+	// Handle worker reg in separate goroutine
+	go func() {
+		for worker := range mr.registerChannel {
+			DPrintf("Master: Registered worker %s\n", worker)
+			workerChannel <- worker
+		}
+	}()
+
+	// Schedule all map tasks
+	for i := 0; i < mr.nMap; i++ {
+		mr.assign(Map, i, mr.nReduce, &wg, workerChannel)
+	}
+	wg.Wait()
+
+	// Schedule all reduce tasks
+	for i := 0; i < mr.nReduce; i++ {
+		mr.assign(Reduce, i, mr.nMap, &wg, workerChannel)
+	}
+	wg.Wait()
+
 	return mr.KillWorkers()
+}
+
+func (mr *MapReduce) assign(jobType JobType, jobNumber int, numOtherPhase int, wg *sync.WaitGroup, workerChannel chan string) {
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		args := DoJobArgs{
+			File:          mr.file,
+			Operation:     jobType,
+			JobNumber:     jobNumber,
+			NumOtherPhase: numOtherPhase,
+		}
+		var reply DoJobReply
+
+		for {
+			select {
+			case worker := <-workerChannel:
+				DPrintf("Assigning %s job %d to worker %s\n", jobType, jobNumber, worker)
+				if call(worker, "Worker.DoJob", &args, &reply) {
+					// job succeeded
+					workerChannel <- worker
+					return
+				}
+				// job failed
+				fmt.Printf("Failed to assign %s job %d to worker %s, retrying...\n", jobType, jobNumber, worker)
+			}
+		}
+	}()
 }
